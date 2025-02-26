acf03e3cf584d4da2f0f554c5a5af43bc9edcafa
KazuyaTTL <pokiwar24h@gmail.com>
2025-02-27 00:03:13 +0700

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,121 @@
+﻿export default [
+	{
+		path: '/user',
+		layout: false,
+		routes: [
+			{
+				path: '/user/login',
+				layout: false,
+				name: 'login',
+				component: './user/Login',
+			},
+			{
+				path: '/user',
+				redirect: '/user/login',
+			},
+		],
+	},
+	{
+		path: '/study-management',
+		name: 'study',
+		icon: 'read',
+		routes: [
+		  {
+			path: '/study-management/subjects',
+			name: 'subjects',
+			component: '@/pages/StudyManagement/Subjects',
+		  },
+		  {
+			path: '/study-management/progress',
+			name: 'progress',
+			component: '@/pages/StudyManagement/Progress',
+		  },
+		  {
+			path: '/study-management/goals',
+			name: 'goals',
+			component: '@/pages/StudyManagement/Goals',
+		  },
+		],
+	  },
+	{
+		path: '/guessing-game',
+		name: 'guessing-game',
+		component: './GuessingGame',
+		access: 'canUser',  // Tùy chỉnh quyền truy cập nếu cần
+	  },
+	///////////////////////////////////
+	// DEFAULT MENU
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: './TrangChu',
+		icon: 'HomeOutlined',
+	},
+	{
+		path: '/gioi-thieu',
+		name: 'About',
+		component: './TienIch/GioiThieu',
+		hideInMenu: true,
+	},
+	{
+		path: '/random-user',
+		name: 'RandomUser',
+		component: './RandomUser',
+		icon: 'ArrowsAltOutlined',
+	},
+
+	// DANH MUC HE THONG
+	// {
+	// 	name: 'DanhMuc',
+	// 	path: '/danh-muc',
+	// 	icon: 'copy',
+	// 	routes: [
+	// 		{
+	// 			name: 'ChucVu',
+	// 			path: 'chuc-vu',
+	// 			component: './DanhMuc/ChucVu',
+	// 		},
+	// 	],
+	// },
+
+	{
+		path: '/notification',
+		routes: [
+			{
+				path: './subscribe',
+				exact: true,
+				component: './ThongBao/Subscribe',
+			},
+			{
+				path: './check',
+				exact: true,
+				component: './ThongBao/Check',
+			},
+			{
+				path: './',
+				exact: true,
+				component: './ThongBao/NotifOneSignal',
+			},
+		],
+		layout: false,
+		hideInMenu: true,
+	},
+	{
+		path: '/',
+	},
+	{
+		path: '/403',
+		component: './exception/403/403Page',
+		layout: false,
+	},
+	{
+		path: '/hold-on',
+		component: './exception/DangCapNhat',
+		layout: false,
+	},
+	{
+		component: './exception/404',
+	},
+	
+];
+
---
+++ src/locales/vi-VN.ts
@@ -0,0 +1,18 @@
+import menu from './vi-VN/menu';
+import pages from './vi-VN/pages';
+
+export default {
+	'app.copyright.produced': 'RIPT',
+	...menu,
+	...pages,
+	'menu.guessing-game': 'Trò Chơi Đoán Số',
+	
+
+  
+  'menu.study': 'Quản lý học tập',
+  'menu.study.subjects': 'Môn học',
+  'menu.study.progress': 'Tiến độ',
+  'menu.study.goals': 'Mục tiêu',
+
+
+};
